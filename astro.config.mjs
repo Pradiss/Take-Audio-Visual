31afa903d42275228306ae414a9f19eb3666f042
Erick <erick@Ericks-MacBook-Air.local>
2026-05-07 10:16:45 -0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,22 @@
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 export default defineConfig({
+  
+
+  integrations: [
+    
+    icon({
+      include: {
+        mdi: ['whatsapp', 'instagram', 'facebook'],
+      },
+    }),
+  ],
+
   vite: {
     plugins: [tailwindcss()],
   },
